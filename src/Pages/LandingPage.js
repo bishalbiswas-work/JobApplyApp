4b3ee8c5a5869c304dfb9f8ef4472d8d0ccff218
bishalbiswas-work <bishalbiswas.work@gmail.com>
2023-12-12 01:47:49 +0530

    path updated

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,9 +1,33 @@
 import React from "react";
-
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import { useNavigate } from "react-router-dom";
 const LandingPage = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <>Landing Page</>
+      <>
+        <Container>
+          <Button
+            onClick={() => {
+              navigate("/job");
+            }}
+          >
+            {" "}
+            Go To Jobs
+          </Button>
+        </Container>
+      </>
     </>
   );
 };
